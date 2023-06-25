c75cae0139e12a65cf23ca97245648d6f8baff35
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2023-06-25 20:12:58 +0100

    populated the database with some samples

--- core/models.py
+++ core/models.py
@@ -5,58 +5,70 @@ from vendor.models import Vendor
 
 # Create your models here.
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=200)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200,blank=False, null=False)
     desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    modified_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     desc = models.TextField()
-    sku = models.CharField(max_length=200)
+    sku = models.CharField(max_length=200, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    categories = models.ManyToManyField(ProductCategory)
+    categories = models.ManyToManyField(ProductCategory, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    modified_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
 class ProductInventory(models.Model):
+    id = models.AutoField(primary_key=True)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    product = models.OneToOneField(
+        Product, on_delete=models.CASCADE, blank=True, null=True
+    )
     quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return  "{} {} remains in {} inventory".format(self.quantity, self.product.name, self.vendor)
 
 class Discount(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=120)
     desc = models.TextField()
     percent = models.DecimalField(max_digits=4, decimal_places=2)
     active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    modified_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name + " " + str(self.percent)
 
 
 class Order(models.Model):
+    id = models.AutoField(primary_key=True)
     items = models.ManyToManyField(Product, through="OrderItem")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    modified_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user + " made an order of {self.total} at {self.updated_at}"
 
 
 class OrderItem(models.Model):
+    id = models.AutoField(primary_key=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
--- vendor/models.py
+++ vendor/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 
 # Create your models here.
 class Vendor(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
     email = models.EmailField(max_length=255)
     store_name = models.CharField(max_length=255,unique=True, blank=False)
     description = models.CharField(max_length=255, blank=True)
@@ -13,17 +14,19 @@ class Vendor(AbstractBaseUser):
     REQUIRED_FIELDS = ['email', 'store_name']
     
     
-    def str(self):
-        self.store_name
+    def __str__(self):
+        return self.store_name
         
 
 class VendorBalance(models.Model):
+    id = models.AutoField(primary_key=True)
     vendor = models.OneToOneField(Vendor, on_delete=models.CASCADE)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.vendor.store_name}'s Balance: {self.balance}"
     
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     
