2ae9c83e2d288ef6bc3c1b0893e0a0157b5c34b4
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2023-07-13 22:38:21 +0100

    create some permission classes

---
+++ authApp/permissions.py
@@ -0,0 +1,107 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import PermissionDenied
+from .models import User
+
+
+
+class IsVendorOrReadOnly(BasePermission):
+    message = "Only Vendor Is Allowed to perform this action"
+    
+    def has_permission(self, request, view):
+        # Allow GET, HEAD, and OPTIONS requests to all users
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        # Check if the user is a vendor
+        if request.user.is_authenticated and request.user.role=="VENDOR":
+            return True
+
+        raise PermissionDenied(self.message)
+    def has_object_permission(self, request, view, obj):
+        # Allow GET, HEAD, and OPTIONS requests to all users
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        # Check if the user is a vendor and owns the product
+        if request.user.is_authenticated and request.user.role=="VENDOR" and obj.vendor == request.user:
+            return True
+        raise PermissionDenied(self.message)
+     
+
+class IsCustomerOrReadOnly(BasePermission):
+    message = "Only Customers Are Allowed to perfor this action"
+    
+    def has_permission(self, request, view):
+        # Allow GET, HEAD, and OPTIONS requests to all users
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        # Check if the user is a customer
+        if request.user.is_authenticated and request.user.role=="CUSTOMER":
+            return True
+        raise PermissionDenied(self.message)
+    
+    def has_object_permission(self, request, view, obj):
+        # Allow GET, HEAD, and OPTIONS requests to all users
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        # Check if the user is a customer and owns the cart
+        if request.user.is_authenticated and request.user.role=="CUSTOMER" and obj.customer == request.user:
+            return True
+        raise PermissionDenied(self.message)
+
+class IsAdminOrReadOnly(BasePermission):
+    message = "Only Admins Are Allowed to perform this action"
+    
+    def has_permission(self, request, view):
+        # Allow GET, HEAD, and OPTIONS requests to all users
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        # Check if the user is an admin
+        if request.user.is_authenticated and request.user.is_admin and request.user.role=="ADMIN":
+            return True
+        raise PermissionDenied(self.message)
+    
+
+    
+class IsVendor(BasePermission):
+    message = "Only Vendor Is Allowed to perform this action"
+    
+    def has_permission(self, request, view):
+        # Check if the user is a vendor
+        if request.user.is_authenticated and request.user.role=="VENDOR":
+            return True
+
+        raise PermissionDenied(self.message)
+    def has_object_permission(self, request, view, obj):
+        # Check if the user is a vendor and owns the product
+        if request.user.is_authenticated and request.user.role=="VENDOR" and obj.vendor == request.user:
+            return True
+        raise PermissionDenied(self.message)
+     
+
+class IsCustomer(BasePermission):
+    message = "Only Customers Are Allowed to perfor this action"
+    
+    def has_permission(self, request, view):
+        # Check if the user is a customer
+        if request.user.is_authenticated and request.user.role=="CUSTOMER":
+            return True
+        raise PermissionDenied(self.message)
+    
+    def has_object_permission(self, request, view, obj):
+        # Check if the user is a customer and owns the cart
+        if request.user.is_authenticated and request.user.role=="CUSTOMER" and obj.customer == request.user:
+            return True
+        raise PermissionDenied(self.message)
+
+class IsAdmin(BasePermission):
+    message = "Only Admins Are Allowed to perform this action"
+    
+    def has_permission(self, request, view):
+        # Check if the user is an admin
+        if request.user.is_authenticated and request.user.is_admin and request.user.role=="ADMIN":
+            return True
+        raise PermissionDenied(self.message)
--- core/viewsets.py
+++ core/viewsets.py
@@ -1,5 +1,9 @@
 from rest_framework import viewsets
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin,
+    RetrieveModelMixin,
+    DestroyModelMixin,
+)
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .serializers import (
     ProductCategorySerializer,
@@ -12,11 +16,13 @@ from .serializers import (
     OrderItemSerializer,
     OrderSerializer,
     UpdateOrderSerializer,
-    
 )
 from .models import Product, ProductCategory, Cart, CartItem, Order
 from authApp.models import User
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from authApp.permissions import IsCustomer, IsVendorOrReadOnly
+from rest_framework.decorators import permission_classes
 
 
 class ProductCategoryViewSet(viewsets.ModelViewSet):
@@ -27,7 +33,8 @@ class ProductCategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
+    permission_classes = [IsVendorOrReadOnly]
+    
     def create(self, serializer):
         vendor_id = self.request.user.id
         quantity = self.request.data.get("quantity")
@@ -55,64 +62,59 @@ class ProductViewSet(viewsets.ModelViewSet):
         return Response(status=201)
 
 
-class CartViewSet(CreateModelMixin,RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
+
+class CartViewSet(
+    CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet
+):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+    permission_classes = []
 
 
 class CartItemViewSet(ModelViewSet):
-    
     http_method_names = ["get", "post", "patch", "delete"]
-    
+
     def get_queryset(self):
         print(self.kwargs)
         return CartItem.objects.filter(cart_id=self.kwargs["cart_pk"])
-    
-    
+
     def get_serializer_class(self):
         if self.request.method == "POST":
             return AddCartItemSerializer
-        
-        elif self.request.method == 'PATCH':
+
+        elif self.request.method == "PATCH":
             return UpdateCartItemSerializer
-        
+
         return CartItemSerializer
-    
+
     def get_serializer_context(self):
         return {"cart_id": self.kwargs["cart_pk"]}
 
 
 class OrderViewSet(ModelViewSet):
-    
     http_method_names = ["get", "patch", "post", "delete", "options", "head"]
-    
+
     # def get_permissions(self):
     #     if self.request.method in ["PATCH", "DELETE"]:
     #         return [IsAdminUser()]
     #     return [IsAuthenticated()]
-            
-    
-    
+
     def create(self, request, *args, **kwargs):
-        serializer = CreateOrderSerializer(data=request.data, context={"owner": self.request.user.id})
+        serializer = CreateOrderSerializer(
+            data=request.data, context={"owner": self.request.user.id}
+        )
         serializer.is_valid(raise_exception=True)
         order = serializer.save()
         serializer = OrderSerializer(order)
         return Response(serializer.data)
-        
-    
 
-    
-    
-    
     def get_serializer_class(self):
-        if self.request.method == 'POST':
+        if self.request.method == "POST":
             return CreateOrderSerializer
-        elif self.request.method == 'PATCH':
+        elif self.request.method == "PATCH":
             return UpdateOrderSerializer
         return OrderSerializer
-       
-    
+
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
