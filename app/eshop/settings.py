aaafb2680e1207eb53013e14922f79c290f16a1c
Usman Abdulsalam <usmanabdulsalamolanrewaju@gmail.com>
2023-09-22 22:16:43 +0100

    hjv

--- app/eshop/settings.py
+++ app/eshop/settings.py
@@ -166,16 +166,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    os.path.join(STATIC_ROOT, "files"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(STATIC_ROOT, "files"),
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
