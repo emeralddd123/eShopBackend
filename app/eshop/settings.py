10c58644d3933a86efd0029dbd330cd9e1182909
Usman Abdulsalam <usmanabdulsalamolanrewaju@gmail.com>
2023-09-22 22:08:03 +0100

    ke

--- app/eshop/settings.py
+++ app/eshop/settings.py
@@ -171,10 +171,10 @@ STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(STATIC_ROOT, "files"),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
