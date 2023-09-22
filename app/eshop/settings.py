fa668127b3ee555b235b43bc5d059f74a85c3bbe
Usman Abdulsalam <usmanabdulsalamolanrewaju@gmail.com>
2023-09-22 23:04:16 +0100

    added email port

--- app/eshop/settings.py
+++ app/eshop/settings.py
@@ -203,7 +203,11 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"  #TODO --Should Wo
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = 587
+if DEBUG:
+    EMAIL_PORT = 587
+else:
+    EMAIL_PORT = os.getenv("EMAIL_PORT")
+
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
